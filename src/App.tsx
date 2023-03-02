46954cd48a9ce3750cbe5fdd88b119237ad39bbe
Arkadiy <alibullterrier@gmail.com>
2023-03-02 08:19:40 +0200

    new li

--- src/App.tsx
+++ src/App.tsx
@@ -6,13 +6,13 @@ function App() {
   return (
     <div className="App">
      <ul>
-      <li></li>
-      <li></li>
-      <li></li>
-      <li></li>
-      <li></li>
-      <li></li>
-      <li></li>
+      <li>1</li>
+      <li>2</li>
+      <li>3</li>
+      <li>4</li>
+      <li>5</li>
+      <li>6</li>
+      <li>7</li>
      </ul>
     </div>
   );
