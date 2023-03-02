ef1942ea28fd73e0702d0c3432dcdd456360fa88
Arkadiy <alibullterrier@gmail.com>
2023-03-02 17:37:52 +0200

    new microtasks

--- src/NewComponent.tsx
+++ src/NewComponent.tsx
@@ -1,41 +1,51 @@
-import React from 'react'
+// import React from 'react'
+
+// type StudentsTypeAll = {
+//   students: Array<StudentsPropsItems>
+// }
+
+// type StudentsPropsItems = {
+//   id: number,
+//   name: string,
+//   age: number
+// }
+
+// const NewComponent = (props: StudentsTypeAll) => {
+//   return (
+//     <ol>
+//       {props.students.map((StudentsCount, index) => {
+//         return <li key={StudentsCount.id}>
+//         {StudentsCount.name}
+//         {StudentsCount.age}
+//         </li>
+//       })}
+//     </ol>
+//   )
+// }
 
 
-type NewComponentType = {
-  students: Array<StudentsType>
+// export default NewComponent;
+
+import React from 'react';
+type CarsProps = {
+  cars: Array<ItemCarProps>
 }
-
-type StudentsType = {
-    id: number,
-    name: string, 
-    age: number,
+type ItemCarProps = {
+  manufacturer: string,
+  model: string
 }
-
-
-
-const NewComponent = (props: NewComponentType) => {
+const NewComponent = (props: CarsProps) => {
   return (
     <ul>
-      {props.students.map((StudentsMapCount, index) => {
-        return <li key={StudentsMapCount.id}>
-          {StudentsMapCount.name}
-          {StudentsMapCount.age}</li>
+      {props.cars.map((CarMap, index) => {
+        return <li key={CarMap.manufacturer}>
+          {CarMap.manufacturer}
+          {CarMap.model}
+          </li>
       })}
-     </ul>
+    </ul>
   )
 }
-
-
-
-
-
-
-
-
-
-
-
-
 
 
 
