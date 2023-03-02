ba2931ebfacc55277023990895af4b1116390abe
Arkadiy <alibullterrier@gmail.com>
2023-03-02 08:43:29 +0200

    type props

--- src/App.tsx
+++ src/App.tsx
@@ -2,16 +2,26 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  const topCars = [
-    {manufacturer:'BMW', model:'m5cs'},
-    {manufacturer:'Mercedes', model:'e63s'},
-    {manufacturer:'Audi', model:'rs6'}
-  ]
+
+type CarsTypeProps = {
+  manufacturer: string,
+  model: string
+};
+const topCars = [
+  {manufacturer:'BMW', model:'m5cs'},
+  {manufacturer:'Mercedes', model:'e63s'},
+  {manufacturer:'Audi', model:'rs6'}
+]
+
+function App(props: CarsTypeProps){
   
   return (
     <div className="App">
-     
+     <ul>
+      <li>{props.model}</li>
+      <li>{props.model}</li>
+      <li>{props.model}</li>
+     </ul>
     </div>
   );
 }
