97bf9f90d89105902d5c0807ca873de1c3f27c0e
Arkadiy <alibullterrier@gmail.com>
2023-03-28 07:48:31 +0300

    new task

--- src/Input.tsx
+++ src/Input.tsx
@@ -9,7 +9,6 @@ type InputPropsType = {
 export const Input = (props: InputPropsType) => {
     const onChangeInputHandler = (event: ChangeEvent<HTMLInputElement>) => {
         props.setTitle(event.currentTarget.value)
-
     }
     return (
         <input value={props.title} onChange={onChangeInputHandler} />
