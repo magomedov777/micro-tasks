2b27a39c9c811fcbd17c65455bb6f0e37df3de4f
Arkadiy <alibullterrier@gmail.com>
2023-04-18 06:52:16 +0300

    add useState

--- src/App.tsx
+++ src/App.tsx
@@ -9,6 +9,7 @@ const App: () => JSX.Element = () => {
     { message: 'message2' },
     { message: 'message3' }
   ]);
+  const [title, setTitle] = useState('');
 
   
   const addMessage = (title: string) => {
