a1e3c90fa13fdc7a71e4951a7aebc9887bfa49ef
Arkadiy <alibullterrier@gmail.com>
2023-04-27 06:31:56 +0300

    new type

--- src/Input.tsx
+++ src/Input.tsx
@@ -1,6 +1,9 @@
 import React, { ChangeEvent } from 'react'
 
-
+type InputPropsType = {
+    setTitle: (title: string) => void
+    title: string
+};
 
 export const Input = (props: InputPropsType) => {
     const onChangeInputHandler = (event: ChangeEvent<HTMLInputElement>) => {
