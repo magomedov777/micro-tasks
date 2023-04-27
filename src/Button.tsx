5563e1bec165e065849711c6ff15a94ae1511c1b
Arkadiy <alibullterrier@gmail.com>
2023-04-27 06:31:06 +0300

    add button type

--- src/Button.tsx
+++ src/Button.tsx
@@ -1,8 +1,9 @@
 import React from 'react'
 
-//universalButton
-
-
+type ButtonPropsType = {
+    callBackAddMessage: () => void
+    name: string
+};
 
 export const Button = (props: ButtonPropsType) => {
     const onClickButtonHandler = () => {
