9084747f1b3ae647107703d12c65145d71b07581
Arkadiy <alibullterrier@gmail.com>
2023-05-27 00:01:08 +0300

    new refactoring, add onKeyPress event, key code

--- src/App.tsx
+++ src/App.tsx
@@ -11,19 +11,20 @@ const App: () => JSX.Element = () => {
   ]);
   const [title, setTitle] = useState('');
 
-  
+
   const addMessage = (title: string) => {
     const newMessage = { message: title }
     setMessage([newMessage, ...message])
   };
   const callBackAddMessage = () => {
+
     addMessage(title)
     setTitle('')
   };
 
   return (
     <div className={'App'}>
-      <Input setTitle={setTitle} title={title} />
+      <Input callBackAddMessage={callBackAddMessage} setTitle={setTitle} title={title} />
       <Button callBackAddMessage={callBackAddMessage} name={'+'} />
       {message.map((el, index) => {
         return (
--- src/Input.tsx
+++ src/Input.tsx
@@ -1,16 +1,22 @@
-import React, { ChangeEvent } from 'react'
+import React, { ChangeEvent, KeyboardEvent } from 'react'
 
 type InputPropsType = {
     setTitle: (title: string) => void
     title: string
+    callBackAddMessage: (title: string) => void
 };
 
 export const Input = (props: InputPropsType) => {
     const onChangeInputHandler = (event: ChangeEvent<HTMLInputElement>) => {
         props.setTitle(event.currentTarget.value)
-
     };
+
+    const onKeyPressHandler = (event: KeyboardEvent<HTMLInputElement>) => {
+        if (event.key === 'Enter') {
+            props.callBackAddMessage(props.title)
+        }
+    }
     return (
-        <input onChange={onChangeInputHandler} value={props.title} />
+        <input onKeyPress={onKeyPressHandler} onChange={onChangeInputHandler} value={props.title} />
     )
 };
